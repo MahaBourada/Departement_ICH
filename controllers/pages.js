b1777142f7da68d92644de133697d2d06bfa806d
Maha Marwa Bourada <mahamarwabourada@gmail.com>
2025-04-17 22:00:39 +0200

    Setup the pages management backend/frontend | Displayed the pages content from the database in HTML format

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,14 +1,69 @@
 import db from "../config/db.js";
+import { v4 as uuidv4 } from "uuid";
 
 export const getAllPages = (req, res) => {
-  db.query(
-    "SELECT * FROM pages, sections_page WHERE pages.idPage = sections_page.idPage",
-    (err, results) => {
+  const sql =
+    "SELECT * FROM pages, sections_page WHERE pages.idPage = sections_page.idPage";
+
+  db.query(sql, (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    } else {
+      res.json(results);
+    }
+  });
+};
+
+export const getPageById = (req, res) => {
+  const { pageTitle } = req.params;
+
+  const sqlPageId = "SELECT * FROM pages WHERE pages.link = ?";
+
+  db.query(sqlPageId, [pageTitle], (err, pageResult) => {
+    if (err) return res.json({ Error: err });
+    if (pageResult.length === 0) return res.json({ Error: err });
+
+    const idPage = pageResult[0].idPage;
+
+    const sql =
+      "SELECT * FROM pages JOIN sections_page ON pages.idPage = sections_page.idPage WHERE pages.idPage = ?";
+
+    db.query(sql, [idPage], (err, results) => {
       if (err) {
         return res.status(500).json({ error: err.message });
       } else {
         res.json(results);
       }
-    }
-  );
+    });
+  });
+};
+
+export const addPage = (req, res) => {
+  const idSection = uuidv4();
+  const sectionBody = req.body;
+
+  const sqlPageId = "SELECT idPage FROM pages WHERE link = ?";
+
+  db.query(sqlPageId, [sectionBody.link], (err, pageResult) => {
+    if (err) return res.json({ Error: err });
+    if (pageResult.length === 0) return res.json({ Error: err });
+
+    const idPage = pageResult[0].idPage;
+
+    const sqlSection =
+      "INSERT INTO departement_ich.sections_page (idSection, texte, ordre_positionnement, idPage) VALUES (?, ?, ?, ?)";
+
+    const values = [
+      idSection,
+      sectionBody.texte,
+      sectionBody.ordre_positionnement,
+      idPage,
+    ];
+
+    db.query(sqlSection, values, (err, result) => {
+      if (err) return res.json({ Error: err });
+
+      return res.json({ Status: "Success" });
+    });
+  });
 };
--- routes/pages.js
+++ routes/pages.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getAllPages } from "../controllers/pages.js";
+import { getAllPages, addPage, getPageById } from "../controllers/pages.js";
 
 const router = express.Router();
 
 router.get("/", getAllPages);
+router.get("/:pageTitle", getPageById);
+router.post("/", addPage);
 
 export default router;
