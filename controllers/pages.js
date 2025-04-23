7e44041acd0b73352e9cd7a695a281e1c87d67cb
Maha Marwa Bourada <mahamarwabourada@gmail.com>
2025-04-23 21:18:16 +0200

    Update pages' content | Translation change/switch

--- controllers/pages.js
+++ controllers/pages.js
@@ -16,6 +16,7 @@ export const getAllPages = (req, res) => {
 
 export const getPageById = (req, res) => {
   const { pageTitle } = req.params;
+  const lang = req.query.lang === "en" ? "en" : "fr";
 
   const sqlPageId = "SELECT * FROM pages WHERE pages.link = ?";
 
@@ -25,8 +26,10 @@ export const getPageById = (req, res) => {
 
     const idPage = pageResult[0].idPage;
 
-    const sql =
-      "SELECT * FROM pages JOIN sections_page ON pages.idPage = sections_page.idPage WHERE pages.idPage = ?";
+    const sql = `SELECT pages.idPage, pages.title, pages.link, sections_page.idSection, sections_page.texte_${lang} AS texte, sections_page.ordre_positionnement 
+                    FROM pages JOIN sections_page 
+                    ON pages.idPage = sections_page.idPage 
+                    WHERE pages.idPage = ?;`;
 
     db.query(sql, [idPage], (err, results) => {
       if (err) {
@@ -51,11 +54,12 @@ export const addPage = (req, res) => {
     const idPage = pageResult[0].idPage;
 
     const sqlSection =
-      "INSERT INTO departement_ich.sections_page (idSection, texte, ordre_positionnement, idPage) VALUES (?, ?, ?, ?)";
+      "INSERT INTO departement_ich.sections_page (idSection, texte_fr, texte_en, ordre_positionnement, idPage) VALUES (?, ?, ?, ?, ?)";
 
     const values = [
       idSection,
-      sectionBody.texte,
+      sectionBody.texte_fr,
+      sectionBody.texte_en,
       sectionBody.ordre_positionnement,
       idPage,
     ];
@@ -63,7 +67,41 @@ export const addPage = (req, res) => {
     db.query(sqlSection, values, (err, result) => {
       if (err) return res.json({ Error: err });
 
-      return res.json({ Status: "Success" });
+      return res.json({ Status: "Success", result });
+    });
+  });
+};
+
+export const editPage = (req, res) => {
+  const idSection = req.params.idSection;
+  const sectionBody = req.body;
+
+  const sqlPageId = "SELECT idPage FROM pages WHERE link = ?";
+
+  db.query(sqlPageId, [sectionBody.link], (err, pageResult) => {
+    if (err) return res.json({ Error: err });
+    if (pageResult.length === 0) return res.json({ Error: err });
+
+    const idPage = pageResult[0].idPage;
+
+    const sqlSection = `
+      UPDATE departement_ich.sections_page
+      SET texte_fr = ?, texte_en = ?, ordre_positionnement = ?, idPage = ?
+      WHERE idSection = ?
+    `;
+
+    const values = [
+      sectionBody.texte_fr,
+      sectionBody.texte_en,
+      sectionBody.ordre_positionnement,
+      idPage,
+      idSection,
+    ];
+
+    db.query(sqlSection, values, (err, result) => {
+      if (err) return res.json({ Error: err });
+
+      return res.json({ Status: "Success", result });
     });
   });
 };
--- routes/pages.js
+++ routes/pages.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { getAllPages, addPage, getPageById } from "../controllers/pages.js";
+import {
+  getAllPages,
+  editPage,
+  getPageById,
+  addPage,
+} from "../controllers/pages.js";
 
 const router = express.Router();
 
 router.get("/", getAllPages);
 router.get("/:pageTitle", getPageById);
 router.post("/", addPage);
+router.put("/:idSection", editPage);
 
 export default router;
