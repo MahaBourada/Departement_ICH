f8bc69c1940f191737a19d1bd5caea37318311ac
Maha Marwa Bourada <mahamarwabourada@gmail.com>
2025-05-17 23:29:01 +0200

    Responsive design page des membres

--- src/pages/User/MembersPage.jsx
+++ src/pages/User/MembersPage.jsx
@@ -22,10 +22,10 @@ const MembersPage = () => {
   }, []);
 
   return (
-    <main className="flex-grow my-10 mb-20 mx-16">
+    <main className="flex-grow my-10 mb-20 mx-16 max-sm:mx-7 max-md:mx-10">
       <nav
-        aria-label="breadcrumb"
-        className="my-1 mb-5 p-1.5 w-fit rounded-xl flex items-center font-medium"
+        aria-label="Fil d'Ariane"
+        className="my-1 mb-5 p-1.5 w-fit rounded-xl flex items-center font-medium max-large-medium:hidden"
       >
         <Link
           to="/"
@@ -51,7 +51,7 @@ const MembersPage = () => {
         Directeur du département
       </h2>
 
-      <div className="grid grid-cols-2 mb-6">
+      <div className="grid grid-cols-2 mb-6 max-md:grid-cols-1">
         {members
           .filter((member) => member.titre === "Directeur du département")
           .map((member) => {
@@ -66,9 +66,9 @@ const MembersPage = () => {
                 key={member.idMembre}
                 to={member.idMembre}
                 state={{ member: member }}
-                className="border-2 border-black rounded-[50px] px-6 py-6 m-4 mx-5 hover:translate-[1px] hover:shadow-none w-full shadow-small"
+                className="border-2 border-black rounded-[50px] px-6 py-6 m-4 mx-5 max-md:mx-auto max-md:w-full hover:translate-[1px] hover:shadow-none w-full shadow-small"
               >
-                <h3 className="font-main font-medium text-header">
+                <h3 className="font-main font-medium text-header max-md:text-3xl">
                   {member.prenom + " " + member.nom.toUpperCase()}
                 </h3>
                 <div className="flex items-start">
@@ -76,10 +76,10 @@ const MembersPage = () => {
                     src="src/assets/images/Anis.png"
                     alt="Photo de Anis ROJBI"
                     width={200}
-                    className="rounded-3xl minimal:hidden"
+                    className="rounded-3xl minimal:hidden max-lg:hidden max-xl:w-40"
                   />
 
-                  <div className="ml-4 my-2">
+                  <div className="ml-4 my-2 max-md:mx-1">
                     <h4 className="font-semibold text-2xl my-2">
                       {member.fonction}
                     </h4>
@@ -104,7 +104,7 @@ const MembersPage = () => {
 
       <h2 className="font-medium text-header font-main mx-2">Administration</h2>
 
-      <div className="grid grid-cols-2 mb-6">
+      <div className="grid grid-cols-2 mb-6 max-md:grid-cols-1">
         {members
           .filter((member) => member.titre === "Administration")
           .map((member) => {
@@ -119,9 +119,9 @@ const MembersPage = () => {
                 key={member.idMembre}
                 to={member.idMembre}
                 state={{ member: member }}
-                className="border-2 border-black rounded-[50px] px-6 py-6 m-4 mx-5 hover:translate-[1px] hover:shadow-none shadow-small"
+                className="border-2 border-black rounded-[50px] px-6 py-6 m-4 mx-5 max-md:mx-auto max-md:w-full hover:translate-[1px] hover:shadow-none shadow-small"
               >
-                <h3 className="font-main font-medium text-header">
+                <h3 className="font-main font-medium text-header max-md:text-3xl">
                   {member.prenom + " " + member.nom.toUpperCase()}
                 </h3>
                 <div className="flex items-start">
@@ -129,10 +129,10 @@ const MembersPage = () => {
                     src="src/assets/images/Anis.png"
                     alt="Photo de Anis ROJBI"
                     width={200}
-                    className="rounded-3xl minimal:hidden"
+                    className="rounded-3xl minimal:hidden max-lg:hidden max-xl:w-40"
                   />
 
-                  <div className="ml-4 my-2">
+                  <div className="ml-4 my-2 max-md:mx-1">
                     <h4 className="font-semibold text-2xl my-2">
                       {member.fonction}
                     </h4>
@@ -157,7 +157,7 @@ const MembersPage = () => {
 
       <h2 className="font-medium text-header font-main mx-2">Enseignants</h2>
 
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-2 max-md:grid-cols-1 gap-y-10">
         {members
           .filter((member) => member.titre === "Enseignant(e)")
           .map((member) => {
@@ -171,36 +171,45 @@ const MembersPage = () => {
               <Link
                 key={member.idMembre}
                 to={member.idMembre}
-                state={{ member: member }}
-                className="border-2 border-black rounded-[50px] px-6 py-6 m-4 mx-5 hover:translate-[1px] hover:shadow-none shadow-small"
+                state={{ member }}
+                className="border-2 border-black rounded-[50px] px-6 py-6 m-4 mx-5 max-md:mx-auto max-md:w-full hover:translate-[1px] hover:shadow-none shadow-small h-full flex flex-col justify-between"
               >
-                <h3 className="font-main font-medium text-header">
+                {/* Title */}
+                <h3 className="font-main font-medium text-header max-md:text-3xl mb-4">
                   {member.prenom + " " + member.nom.toUpperCase()}
                 </h3>
-                <div className="flex items-start">
-                  <img
-                    src="src/assets/images/Anis.png"
-                    alt="Photo de Anis ROJBI"
-                    width={200}
-                    className="rounded-3xl minimal:hidden"
-                  />
 
-                  <div className="ml-4 my-2">
-                    <h4 className="font-semibold text-2xl my-2">
-                      {member.fonction}
-                    </h4>
-                    {member.section && (
-                      <>
-                        <p className="mx-2">{mainPart}</p>
-                        <ul className="list-disc mx-9 leading-9">
-                          {subParts.map((item, index) => (
-                            <li key={index}>
-                              {item.charAt(0).toLowerCase() + item.slice(1)}
-                            </li>
-                          ))}
-                        </ul>
-                      </>
-                    )}
+                {/* Content section */}
+                <div className="flex-grow flex flex-col justify-between h-full">
+                  {/* Text + image */}
+                  <div className="flex justify-between h-full gap-4">
+                    {/* Image (stick to bottom visually) */}
+                    <div className="flex flex-col justify-end">
+                      <img
+                        src="src/assets/images/Anis.png"
+                        alt={`Photo de ${member.nom}`}
+                        width={200}
+                        className="rounded-3xl minimal:hidden max-lg:hidden max-xl:w-40"
+                      />
+                    </div>
+                    {/* Textual info */}
+                    <div className="flex flex-col justify-start flex-grow">
+                      <h4 className="font-semibold text-2xl mb-2">
+                        {member.fonction}
+                      </h4>
+                      {member.section && (
+                        <>
+                          <p className="mx-2">{mainPart}</p>
+                          <ul className="list-disc mx-9 leading-9">
+                            {subParts.map((item, index) => (
+                              <li key={index}>
+                                {item.charAt(0).toLowerCase() + item.slice(1)}
+                              </li>
+                            ))}
+                          </ul>
+                        </>
+                      )}
+                    </div>
                   </div>
                 </div>
               </Link>
