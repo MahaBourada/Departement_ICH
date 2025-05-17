ed1cb329c47c581fbd668a756f9ebf38ae4e11c1
Maha Marwa Bourada <mahamarwabourada@gmail.com>
2025-05-17 16:21:15 +0200

    Responsive design page de conférences

--- src/pages/User/ConferencesPage.jsx
+++ src/pages/User/ConferencesPage.jsx
@@ -7,10 +7,10 @@ const ConferencesPage = () => {
   const { t } = useTranslation();
 
   return (
-    <main className="flex-grow my-10 mb-20 mx-16">
+    <main className="flex-grow my-10 mb-20 mx-16 max-sm:mx-7 max-md:mx-10">
       <nav
-        aria-label="breadcrumb"
-        className="my-1 mb-5 p-1.5 w-fit rounded-xl flex items-center font-medium"
+        aria-label="Fil d'Ariane"
+        className="my-1 mb-5 p-1.5 w-fit rounded-xl flex items-center font-medium max-large-medium:hidden"
       >
         <Link
           to="/"
@@ -23,9 +23,7 @@ const ConferencesPage = () => {
           {t("news.link")}
         </span>
         <ChevronRight size={33} color="#232323" strokeWidth={2} />
-        <span
-          className="px-4 py-2 rounded-xl hover:text-dark-accent hover:bg-bg-transparent hover:underline hover:translate-[1px]"
-        >
+        <span className="px-4 py-2 rounded-xl hover:text-dark-accent hover:bg-bg-transparent hover:underline hover:translate-[1px]">
           {t("news.conferences.title")}
         </span>
       </nav>
@@ -34,13 +32,13 @@ const ConferencesPage = () => {
         {t("news.conferences.title")}
       </h1>
 
-      <div>
-        <h2 className="font-main font-medium text-header mx-4">
+      <div className="mx-4 max-md:mx-2">
+        <h2 className="font-main font-medium text-header">
           Prochaine conférence
         </h2>
 
         <div className="flex">
-          <div className="w-[55%] minimal:w-full mr-12 minimal:mr-4 ml-6">
+          <div className="w-[62%] max-large-large:w-full minimal:w-full mr-12 minimal:mr-4 ml-6 max-md:mx-2">
             <div className="h-[0.5px] bg-black my-5 w-full"></div>
 
             <div className="text-body">
@@ -88,100 +86,101 @@ const ConferencesPage = () => {
             src="assets/vectors/Conferences.svg"
             alt=""
             role="presentation"
-            className="minimal:hidden"
-            width={500}
+            className="minimal:hidden max-large-large:hidden m-auto"
+            width={350}
           />
         </div>
       </div>
 
-      <div className="mr-10">
-        <h2 className="font-main font-medium text-header mx-4">
+      <div className="mr-10 mx-4 max-md:mr-0 max-md:mx-2">
+        <h2 className="font-main font-medium text-header">
           Conférences passées
         </h2>
 
-        <div className="h-[0.5px] bg-black my-5 w-full mx-6"></div>
+        <div className="mx-6 max-md:mx-2">
+          <div className="h-[0.5px] bg-black my-5 w-full"></div>
 
-        <div className="text-body w-full mx-6">
-          <div className="flex items-start justify-between">
-            <div>
-              <h3 className="font-semibold my-2">
-                Yousra Ben Jmaa (Laboratoire Signaux et Smart Systèmes, Enis,
-                Tunis)
-              </h3>
+          <div className="text-body w-full">
+            <div className="flex items-start justify-between">
+              <div>
+                <h3 className="font-semibold my-2">
+                  Yousra Ben Jmaa (Laboratoire Signaux et Smart Systèmes, Enis,
+                  Tunis)
+                </h3>
 
-              <p className="my-2">
-                Vision par ordinateur au service des personnes handicapées
-              </p>
-              <p className="font-semibold my-2">17h-19h30</p>
+                <p className="my-2">
+                  Vision par ordinateur au service des personnes handicapées
+                </p>
+                <p className="font-semibold my-2">17h-19h30</p>
+              </div>
+
+              <div>
+                <p className="font-semibold text-end my-2">
+                  13 février 2025
+                  <br />
+                  20 février 2025
+                </p>
+
+                <ul className="text-end my-2">
+                  <li>Université Paris 8</li>
+                  <li>Bâtiment C</li>
+                  <li>Salle C223</li>
+                </ul>
+              </div>
             </div>
 
-            <div>
-              <p className="font-semibold text-end my-2">
-                13 février 2025
-                <br />
-                20 février 2025
-              </p>
-
-              <ul className="text-end my-2">
-                <li>Université Paris 8</li>
-                <li>Bâtiment C</li>
-                <li>Salle C223</li>
-              </ul>
-            </div>
+            <p className="my-2">
+              Dans un premier lieu je présenterai des généralités sur la vision
+              par ordinateurs (principes, modèles, étapes et applications). Dans
+              un deuxième temps je présenterai les techniques de détection et
+              reconnaissance d'objets et je terminerai par le suivi dans les
+              séquences vidéos. Je donnerai à chaque fois des exemples liés au
+              handicap.
+            </p>
           </div>
 
-          <p className="my-2">
-            Dans un premier lieu je présenterai des généralités sur la vision
-            par ordinateurs (principes, modèles, étapes et applications). Dans
-            un deuxième temps je présenterai les techniques de détection et
-            reconnaissance d'objets et je terminerai par le suivi dans les
-            séquences vidéos. Je donnerai à chaque fois des exemples liés au
-            handicap.
-          </p>
-        </div>
+          <div className="h-[0.5px] bg-black my-5 w-full"></div>
 
-        <div className="h-[0.5px] bg-black my-5 w-full mx-6"></div>
+          <div className="text-body w-full">
+            <div className="flex items-start justify-between">
+              <div>
+                <h3 className="font-semibold my-2">
+                  Yousra Ben Jmaa (Laboratoire Signaux et Smart Systèmes, Enis,
+                  Tunis)
+                </h3>
 
-        <div className="text-body w-full mx-6">
-          <div className="flex items-start justify-between">
-            <div>
-              <h3 className="font-semibold my-2">
-                Yousra Ben Jmaa (Laboratoire Signaux et Smart Systèmes, Enis,
-                Tunis)
-              </h3>
+                <p className="my-2">
+                  Vision par ordinateur au service des personnes handicapées
+                </p>
+                <p className="font-semibold my-2">17h-19h30</p>
+              </div>
 
-              <p className="my-2">
-                Vision par ordinateur au service des personnes handicapées
-              </p>
-              <p className="font-semibold my-2">17h-19h30</p>
+              <div>
+                <p className="font-semibold text-end my-2">
+                  13 février 2025
+                  <br />
+                  20 février 2025
+                </p>
+
+                <ul className="text-end my-2">
+                  <li>Université Paris 8</li>
+                  <li>Bâtiment C</li>
+                  <li>Salle C223</li>
+                </ul>
+              </div>
             </div>
 
-            <div>
-              <p className="font-semibold text-end my-2">
-                13 février 2025
-                <br />
-                20 février 2025
-              </p>
-
-              <ul className="text-end my-2">
-                <li>Université Paris 8</li>
-                <li>Bâtiment C</li>
-                <li>Salle C223</li>
-              </ul>
-            </div>
+            <p className="my-2">
+              Dans un premier lieu je présenterai des généralités sur la vision
+              par ordinateurs (principes, modèles, étapes et applications). Dans
+              un deuxième temps je présenterai les techniques de détection et
+              reconnaissance d'objets et je terminerai par le suivi dans les
+              séquences vidéos. Je donnerai à chaque fois des exemples liés au
+              handicap.
+            </p>
           </div>
-
-          <p className="my-2">
-            Dans un premier lieu je présenterai des généralités sur la vision
-            par ordinateurs (principes, modèles, étapes et applications). Dans
-            un deuxième temps je présenterai les techniques de détection et
-            reconnaissance d'objets et je terminerai par le suivi dans les
-            séquences vidéos. Je donnerai à chaque fois des exemples liés au
-            handicap.
-          </p>
+          <div className="h-[0.5px] bg-black my-5 w-full"></div>
         </div>
-
-        <div className="h-[0.5px] bg-black my-5 w-full mx-6"></div>
       </div>
     </main>
   );
